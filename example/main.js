cd4c1253a8208b2d99a692cc6899c066e5aab7af
Λ B H I N Λ V <ixd.abhi@gmail.com>
2022-09-18 17:35:11 +0530

    fixed the example

--- example/main.js
+++ example/main.js
@@ -1,30 +1,46 @@
 /*
  Views
 */
+const catsView = document.getElementById('cats')
 const appleView = document.getElementById('apple')
 const bananaView = document.getElementById('banana')
-const catsView = document.getElementById('cats')
 
-const btn = document.getElementById('btn') 
+const btnCats = document.getElementById('btnCats')
+const btnApple = document.getElementById('btnApple')
+const btnBanan = document.getElementById('btnBanana')
 
 /*
 Initializing View Model here 
 */
-const myviewmodel = new MyViewModel()
-const cats = new LiveObserver()
+const cats = new LiveObserver() // example type 1 
+const myviewmodel = new MyViewModel() // example type 2 where all the variables are encapsulated inside a parent class 
  
 /*
 Modifying/Changing values on button click so that other can observe the change in the value
 */
-btn.addEventListener("click", (e) => { 
-    myviewmodel.apple.setValue("Apple: " + Math.random()*100)
-    myviewmodel.banana.setValue("Banana: " + Math.random() * 100)
+btnCats.addEventListener("click", (e) => {
     cats.setValue("Cats: " + Math.random() * 100) 
+})
+
+btnApple.addEventListener("click", (e) => {
+    myviewmodel.apple.setValue("Apple: " + Math.random() * 100) 
+})
+
+btnBanan.addEventListener("click", (e) => {
+    myviewmodel.banana.setValue("Banana: " + Math.random() * 100) 
 })
 
 /*
  Observing values and updating the views, here I have used span as an example
 */
+
+// example 1 observer
+cats.observe(data => {
+    catsView.innerText = makeid(10)
+    console.log('banana->', data)
+})
+
+// example 2 observers
 myviewmodel.apple.observe(data => {
     appleView.innerText = data
     console.log('apple->',data)
@@ -35,10 +51,17 @@ myviewmodel.banana.observe(data => {
     console.log('banana->', data)
 })
 
-cats.observe(data => {
-    catsView.innerText = data
-    console.log('banana->', data)
-})
 
 
- 
+
+/*---------------------------------------------*/
+ function makeid(length) {
+     var result = '';
+     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+     var charactersLength = characters.length;
+     for (var i = 0; i < length; i++) {
+         result += characters.charAt(Math.floor(Math.random() *
+             charactersLength));
+     }
+     return result;
+ }
